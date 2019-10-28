1fff453a26f0906a1f6b764cfd7e33a2ac05b403
mttpisi <matteo.pisi@gmail.com>
2019-10-28 14:10:47 +0100

    Completato ma fa schifo

--- main.js
+++ main.js
@@ -3,26 +3,80 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var users = require("./modules/users");
 var utenti = [];
 var errorFlag = true;
-try {
-    utenti.push(new users.UtenteNewsletter("email@email.it", true));
-    utenti.push(new users.UtenteInteressato("email2@email.it", true, "Marco", "Rossi", "3399990000"));
-    utenti.push(new users.UtenteSelezioni("email3@email.it", true, "Giuseppe", "Bianchi", "3388880000", "CodiceFiscale13", "CartaIdentita", "Diploma", "Curriculum", "Lettera"));
-    utenti.push(new users.UtenteGraduatoria("email4@email.it", true, "Giovanni", "Verdi", "3377770000", "CodiceFiscale123", "CartaIdentita", "Diploma", "Curriculum", "Lettera", 30, 25));
-    utenti.push(new users.Iscritto("email5@email.it", true, "Maria", "Azzurri", "3366660000", "CodiceFiscale123", "CartaIdentita", "Diploma", "Curriculum", "Lettera", 30, 25));
-    utenti.push(new users.Allievo("email6@email.it", true, "Giacomo", "Arancioni", "3355550000", "CodiceFiscale123", "CartaIdentita", "Diploma", "Curriculum", "Lettera", 30, 25, "PasswordStraSegreta"));
-    utenti.push(new users.Docente("emailDoc@email.it", true, 2000));
-}
-catch (e) {
-    console.log(e);
-    errorFlag = false;
-}
-if (errorFlag) {
-    for (var i = 0; i < utenti.length; i++) {
-        console.log(utenti[i].stringTipoUtente());
+utenti.push(new users.UtenteNewsletter("email@email.it", true));
+utenti.push(new users.UtenteInteressato("email2@email.it", true, "Marco", "Rossi", "3399990000"));
+utenti.push(new users.UtenteSelezioni("email3@email.it", true, "Giuseppe", "Bianchi", "3388880000", "CodiceFiscale123", "CartaIdentita", "Diploma", "Curriculum", "Lettera"));
+utenti.push(new users.UtenteGraduatoria("email4@email.it", true, "Giovanni", "Verdi", "3377770000", "CodiceFiscale123", "CartaIdentita", "Diploma", "Curriculum", "Lettera", 30, 25));
+utenti.push(new users.Iscritto("email5@email.it", true, "Maria", "Azzurri", "3366660000", "CodiceFiscale123", "CartaIdentita", "Diploma", "Curriculum", "Lettera", 30, 25));
+utenti.push(new users.Allievo("email6@email.it", true, "Giacomo", "Arancioni", "3355550000", "CodiceFiscale123", "CartaIdentita", "Diploma", "Curriculum", "Lettera", 30, 25, "PasswordStraSegreta"));
+utenti.push(new users.Docente("emailDoc@email.it", true, 2000));
+console.log(utenti);
+console.log("\n\n\n\n");
+var res = lookFor(utenti, "nome", "Marco");
+var res1 = lookFor(utenti, "cognome", "Bianchi");
+var res2 = lookFor(utenti, "email", "emailDoc@email.it");
+var res3 = lookFor(utenti, "telefono", "3366660000");
+var res4 = lookFor(utenti, "codicefiscale", "CodiceFiscale123");
+/* console.log(res);
+console.log("1-_-_-_-");
+console.log(res1);
+console.log("2-_-_-_-"); */
+console.log(res2);
+console.log("3-_-_-_-");
+/* console.log(res3);
+console.log("4-_-_-_-");
+console.log(res4); */
+function lookFor(array, type, searchValue) {
+    var results = [];
+    switch (type) {
+        case "email":
+            for (var i = 0; i < array.length; i++) {
+                if (array[i].email._email == searchValue)
+                    results.push(array[i]);
+            }
+            return results;
+            break;
+        case "nome":
+            for (var i = 0; i < array.length; i++) {
+                if (array[i].stringTipoUtente() != "NEWSLETTER" && array[i].stringTipoUtente() != "DOCENTE") {
+                    if (array[i].nome == searchValue)
+                        results.push(array[i]);
+                }
+            }
+            return results;
+            break;
+        case "cognome":
+            for (var i = 0; i < array.length; i++) {
+                if (array[i].stringTipoUtente() != "NEWSLETTER" && array[i].stringTipoUtente() != "DOCENTE") {
+                    if (array[i].cognome == searchValue)
+                        results.push(array[i]);
+                }
+            }
+            return results;
+            break;
+        case "telefono":
+            for (var i = 0; i < array.length; i++) {
+                if (array[i].stringTipoUtente() != "NEWSLETTER" && array[i].stringTipoUtente() != "DOCENTE") {
+                    if (array[i].telefono._numero == searchValue)
+                        results.push(array[i]);
+                }
+            }
+            return results;
+            break;
+        case "codicefiscale":
+            for (var i = 0; i < array.length; i++) {
+                if (array[i].stringTipoUtente() != "NEWSLETTER" && array[i].stringTipoUtente() != "INTERESSATO" && array[i].stringTipoUtente() != "DOCENTE") {
+                    if (array[i].CF._codice == searchValue)
+                        results.push(array[i]);
+                }
+            }
+            return results;
+            break;
+        default:
+            break;
     }
-    console.log(utenti[0].email);
+    return results;
 }
-console.log("fine");
 //RIFARE TUTTO DA 0!
 /* function addUtente(tipoUtente:string,email:string,privacy:boolean,nome?:string,cognome?:string,telefono?:string,CF?:string,CI:string,titoloStudio:string,CV:string,lettera:string,puntTest:number,puntColloquio:number,password:string,){
     switch (tipoUtente) {
--- main.ts
+++ main.ts
@@ -4,24 +4,85 @@ import * as userData from "./modules/usersData";
 const utenti: any = [];
 var errorFlag:boolean = true;
 
-try{
-    utenti.push(new users.UtenteNewsletter("email@email.it",true));
-    utenti.push(new users.UtenteInteressato("email2@email.it",true,"Marco","Rossi","3399990000"));
-    utenti.push(new users.UtenteSelezioni("email3@email.it",true,"Giuseppe","Bianchi","3388880000","CodiceFiscale123","CartaIdentita","Diploma","Curriculum","Lettera"));
-    utenti.push(new users.UtenteGraduatoria("email4@email.it",true,"Giovanni","Verdi","3377770000","CodiceFiscale123","CartaIdentita","Diploma","Curriculum","Lettera",30,25));
-    utenti.push(new users.Iscritto("email5@email.it",true,"Maria","Azzurri","3366660000","CodiceFiscale123","CartaIdentita","Diploma","Curriculum","Lettera",30,25));
-    utenti.push(new users.Allievo("email6@email.it",true,"Giacomo","Arancioni","3355550000","CodiceFiscale123","CartaIdentita","Diploma","Curriculum","Lettera",30,25,"PasswordStraSegreta"));
-    utenti.push(new users.Docente("emailDoc@email.it",true,2000));
-}catch(e){
-    console.log(e);
-    errorFlag = false;
-}
 
-if(errorFlag){
-    for(let i = 0; i<utenti.length;i++){
-        console.log(utenti[i].stringTipoUtente());
+utenti.push(new users.UtenteNewsletter("email@email.it",true));
+utenti.push(new users.UtenteInteressato("email2@email.it",true,"Marco","Rossi","3399990000"));
+utenti.push(new users.UtenteSelezioni("email3@email.it",true,"Giuseppe","Bianchi","3388880000","CodiceFiscale123","CartaIdentita","Diploma","Curriculum","Lettera"));
+utenti.push(new users.UtenteGraduatoria("email4@email.it",true,"Giovanni","Verdi","3377770000","CodiceFiscale123","CartaIdentita","Diploma","Curriculum","Lettera",30,25));
+utenti.push(new users.Iscritto("email5@email.it",true,"Maria","Azzurri","3366660000","CodiceFiscale123","CartaIdentita","Diploma","Curriculum","Lettera",30,25));
+utenti.push(new users.Allievo("email6@email.it",true,"Giacomo","Arancioni","3355550000","CodiceFiscale123","CartaIdentita","Diploma","Curriculum","Lettera",30,25,"PasswordStraSegreta"));
+utenti.push(new users.Docente("emailDoc@email.it",true,2000));
+
+console.log(utenti);
+
+console.log("\n\n\n\n")
+
+let res: any[] = lookFor(utenti,"nome","Marco");
+let res1: any[] = lookFor(utenti,"cognome","Bianchi");
+let res2: any[] = lookFor(utenti,"email","emailDoc@email.it");
+let res3: any[] = lookFor(utenti,"telefono","3366660000");
+let res4: any[] = lookFor(utenti,"codicefiscale","CodiceFiscale123");
+
+console.log(res);
+console.log("1-_-_-_-");
+console.log(res1); 
+console.log("2-_-_-_-");
+console.log(res2); 
+console.log("3-_-_-_-");
+console.log(res3); 
+console.log("4-_-_-_-");
+console.log(res4); 
+
+function lookFor(array:any[], type:string, searchValue:string):any[]{
+    let results:any[] = [];
+    switch(type){
+        case "email":
+            for(let i = 0; i<array.length;i++){
+                if(array[i].email._email == searchValue) results.push(array[i]);
+            }
+            return results;
+            break;
+
+        case "nome":
+            for(let i = 0; i<array.length;i++){
+                if(array[i].stringTipoUtente() != "NEWSLETTER" && array[i].stringTipoUtente() != "DOCENTE"){   
+                    if(array[i].nome == searchValue) results.push(array[i]);
+                }
+            }
+            return results;
+            break;
+
+        case "cognome":
+            for(let i = 0; i<array.length;i++){
+                if(array[i].stringTipoUtente() != "NEWSLETTER" && array[i].stringTipoUtente() != "DOCENTE"){   
+                    if(array[i].cognome == searchValue) results.push(array[i]);
+                }
+            }
+            return results;
+            break;
+
+        case "telefono":
+            for(let i = 0; i<array.length;i++){
+                if(array[i].stringTipoUtente() != "NEWSLETTER" && array[i].stringTipoUtente() != "DOCENTE"){   
+                    if(array[i].telefono._numero == searchValue) results.push(array[i]);
+                }
+            }
+            return results;
+            break;
+
+        case "codicefiscale":
+            for(let i = 0; i<array.length;i++){
+                if(array[i].stringTipoUtente() != "NEWSLETTER" && array[i].stringTipoUtente() != "INTERESSATO" && array[i].stringTipoUtente() != "DOCENTE"){   
+                    if(array[i].CF._codice == searchValue) results.push(array[i]);
+                }
+            }
+            return results;
+            break;
+        
+        default:
+            break;
     }
-    console.log(utenti[0].email);
+    return results;
 }
 
 //RIFARE TUTTO DA 0!
